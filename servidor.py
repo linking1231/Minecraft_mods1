b35aad9ef81670c8a644e69fd7f626c3f88a5ab6
linking1231 <163462995+linking1231@users.noreply.github.com>
2024-06-18 15:59:45 -0600

    probando

---
+++ servidor.py
@@ -0,0 +1,75 @@
+# Versiones disponibles
+# forge, mohist, fabric, vanilla, paper
+
+# Puedes instalar mohist después de instalar forge desde el menú de gestionar
+# Puedes instalar paper después de instalar vanilla desde el menú de gestionar
+# Puedes instalar purpur después de instalar fabric desde el menú de gestionar
+
+# Regiones de ngrok
+# Código          Lugar
+#-----------      ---------------------------
+# ap	          Asia / Pacífico (Singapore)
+# au		      Australia (Sydney)
+# eu		      Europa (Frankfurt)
+# in		      India (Mumbai)
+# jp		      Japón (Tokyo)
+# sa		      Sudamérica (São Paulo)
+# us		      Estados unidos (Ohio)
+# us-cal-1	      Estados unidos (California)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# No toques nada de aquí para abajo, puedes dañarlo
+import requests,os,base64
+if not os.path.exists("./.gitignore"):
+    big = "L1B5dGhvbioNCi93b3JrX2FyZWEqDQovc2Vydmlkb3JfbWluZWNyYWZ0DQovbWluZWNyYWZ0X3NlcnZlcg0KL3NlcnZpZG9yX21pbmVjcmFmdF9vbGQNCi90YWlsc2NhbGUtY3MNCi90aGFub3MNCi9zZXJ2ZXJzDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioubXNwDQoqLm91dHB1dA=="
+    dec = base64.standard_b64decode(big).decode()
+    with open(".gitignore", 'w') as giti:
+        giti.write(dec)
+
+def is_file_in_use(filepath):
+    try:
+        # Intenta renombrar el archivo para ver si está en uso
+        os.rename(filepath, filepath)
+        return False
+    except OSError:
+        return True
+
+def download_latest_release(download_path='.'):
+    mirror = "https://elyxdev.github.io/latest"
+    pet = requests.get(mirror)
+    if pet.status_code == 200:
+        data = pet.json()
+        url = data.get('url')
+        version = url.split("/")[-1]
+        pathto = os.path.join(download_path, version)
+        
+        # Descarga el contenido del archivo
+        content = requests.get(url).content
+
+        # Espera hasta que el archivo no esté en uso
+        while is_file_in_use(pathto):
+            time.sleep(1)  # Espera 1 segundo antes de volver a comprobar
+
+        # Ahora abre el archivo para escritura binaria
+        with open(pathto, 'wb') as archivo:
+            archivo.write(content)
+
+        return version
+
+flnm = download_latest_release()
+if flnm.split(".")[-1] == "pyc":
+    os.system(f"python3 {flnm}")
+else:
+    os.system(f"chmod +x {flnm} && ./{flnm}")
